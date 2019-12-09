c0890eead48c282d0feb849537b9b086df83e5af
bai <1335239218@qq.com>
2019-12-09 20:34:59 +0800

    2

--- my_wifie_django_taobao/myshop/urls.py
+++ my_wifie_django_taobao/myshop/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('products/admin/', admin.site.urls),
-    path('products/', include('products_app.url')),
+    path('api_shop/admin/', admin.site.urls),
+    path('api_shop/', include('products_app.url')),
 ]
